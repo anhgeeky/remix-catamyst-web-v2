1f39ebad6d33df5e15920c387ad91298000b2262
M Haidar Hanif <git@mhaidarhanif.com>
2021-03-15 20:04:45 +0700

    chore: generate sitemap and other config

---
+++ next-sitemap.js
@@ -0,0 +1,24 @@
+const excludes = [
+  '/404',
+  '/cms',
+  '/dashboard',
+  '/onboard',
+  '/reset',
+  '/secret',
+  '/settings',
+]
+
+module.exports = {
+  siteUrl: 'https://catamyst.com',
+  generateRobotsTxt: true,
+  exclude: excludes,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: excludes,
+      },
+    ],
+  },
+}
