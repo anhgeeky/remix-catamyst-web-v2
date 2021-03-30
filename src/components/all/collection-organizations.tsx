332a107e4fd6c99657b84547d7c4b1695ce29c22
M Haidar Hanif <git@mhaidarhanif.com>
2021-03-31 06:50:28 +0700

    feat: filter isPublished organizations

--- src/components/all/collection-organizations.tsx
+++ src/components/all/collection-organizations.tsx
@@ -27,52 +27,54 @@ import { OrganizationAvatar } from '@components/organizations'
 export function CollectionOrganizations({ organizations }) {
   return (
     <SimpleGrid spacing={3} width="100%" minChildWidth={250}>
-      {organizations.map((org, index) => {
-        return (
-          <NextLink key={org.handle} href={org.handle} passHref>
-            <Link
-              rounded="md"
-              p={3}
-              _hover={{ bg: useColorModeValue('gray.100', 'gray.800') }}
-            >
-              <Stack
-                className="org-and-contents"
-                maxW="200px"
-                direction={{ base: 'column', lg: 'row' }}
+      {organizations
+        .filter((org) => org.isPublished)
+        .map((org, index) => {
+          return (
+            <NextLink key={org.handle} href={org.handle} passHref>
+              <Link
+                rounded="md"
+                p={3}
+                _hover={{ bg: useColorModeValue('gray.100', 'gray.800') }}
               >
-                <HStack minW={300} spacing={3}>
-                  <OrganizationAvatar org={org} />
-                  <Stack spacing={1}>
-                    <Flex id="org-name-verified">
-                      <Heading className="org-name" as="h3" size="md">
-                        {org.name}
-                      </Heading>
-                      <chakra.span
-                        color="teal.500"
-                        position="relative"
-                        top="2px"
-                        ml={1}
+                <Stack
+                  className="org-and-contents"
+                  maxW="200px"
+                  direction={{ base: 'column', lg: 'row' }}
+                >
+                  <HStack minW={300} spacing={3}>
+                    <OrganizationAvatar org={org} />
+                    <Stack spacing={1}>
+                      <Flex id="org-name-verified">
+                        <Heading className="org-name" as="h3" size="md">
+                          {org.name}
+                        </Heading>
+                        <chakra.span
+                          color="teal.500"
+                          position="relative"
+                          top="2px"
+                          ml={1}
+                        >
+                          {org.isVerified && <Icon name="verified" />}
+                        </chakra.span>
+                      </Flex>
+                      <Heading
+                        className="org-handle"
+                        as="h4"
+                        size="sm"
+                        color="gray.500"
+                        fontFamily="body"
+                        fontWeight="400"
                       >
-                        {org.isVerified && <Icon name="verified" />}
-                      </chakra.span>
-                    </Flex>
-                    <Heading
-                      className="org-handle"
-                      as="h4"
-                      size="sm"
-                      color="gray.500"
-                      fontFamily="body"
-                      fontWeight="400"
-                    >
-                      @{org.handle}
-                    </Heading>
-                  </Stack>
-                </HStack>
-              </Stack>
-            </Link>
-          </NextLink>
-        )
-      })}
+                        @{org.handle}
+                      </Heading>
+                    </Stack>
+                  </HStack>
+                </Stack>
+              </Link>
+            </NextLink>
+          )
+        })}
     </SimpleGrid>
   )
 }
