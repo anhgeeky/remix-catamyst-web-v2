a0fc3c1039147f596da30a29d24ecdd3e864c239
M Haidar Hanif <git@mhaidarhanif.com>
2021-04-11 05:40:19 +0700

    fix: git ignore, add error page

---
+++ src/pages/_error.tsx
@@ -0,0 +1,16 @@
+function errorPage({ statusCode }) {
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : 'An error occurred on client'}
+    </p>
+  )
+}
+
+errorPage.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
+
+export default Error
