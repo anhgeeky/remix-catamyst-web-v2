a4d5cfa024ef804b1594d762651e3f1bb3d40496
M Haidar Hanif <git@mhaidarhanif.com>
2022-12-08 13:48:14 +0700

    feat: update user avatar component logic

--- src/components/users/avatar.tsx
+++ src/components/users/avatar.tsx
@@ -3,6 +3,19 @@ import { Avatar, Box, useColorModeValue } from '@chakra-ui/react'
 
 export function UserAvatar({ profile, size = 100, textSize = '2xl' }) {
   return (
+    <Avatar
+      name={profile.name}
+      src={profile.avatar_url}
+      width={size}
+      height={size}
+      size={textSize}
+      rounded="full"
+    />
+  )
+}
+
+export function LegacyUserAvatar({ profile, size = 100, textSize = '2xl' }) {
+  return (
     <>
       {!profile.avatar_url && (
         <Avatar
